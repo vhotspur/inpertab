8c58d469d861874cd398daf7d60cf2d2d579a729
Vojtech Horky <vojtech.horky@gmail.com>
2010-09-20 13:00:17 +0200

    Nicer element details dialog

--- pte.js
+++ pte.js
@@ -100,7 +100,7 @@ function PeriodicTable() {
 		this._elementDetails("name", info.localName);
 		this._elementDetails("atomic-number", info.atomicNumber);
 		this._elementDetails("mass", info.mass);
-		this._elementDetails("universal-name", "&lt;unknown&gt;");
+		this._elementDetails("universal-name", info.universalName);
 		this._elementDetails("english-name", info.englishName);
 		this._elementDetails("period", info.period);
 		this._elementDetails("group", info.group);
@@ -120,11 +120,16 @@ function PeriodicTable() {
 			this._elementDetails("melting-point-celsius", info.meltingPoint - 273);
 		}
 		
+		var dialogTitle = info.universalName + " (" + info.localName + ")";
+		if (dialogTitle == " ()") {
+			dialogTitle = info.englishName;
+		}
+		
 		$("#dialog-element-details").dialog({
 			modal: true,
 			buttons: { "Close": function() { $(this).dialog("close"); } },
 			width: 500,
-			title: info[1]
+			title:  dialogTitle
 		});
 	};
 }
